e5c794d1205f6e7f64bc27ff687d207e786965df
yuri_sn <yuri.snitko@gmail.com>
2024-11-02 20:26:31 +0300

    add changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,106 +1,38 @@
-'use client';
 import NewTask from '@/components/newTask/newtask';
 import Image from 'next/image';
 import Task from '@/components/task/task';
 import Modal from '@/components/modal/modal';
-import { useState, Dispatch, SetStateAction, useEffect } from 'react';
-import { createUser, getUsersTask, getAllUsers } from './lib/actions';
+import CreateUsersForm from '@/components/createUsersForm/createUsersForm';
+
+import { createUser, getUsersTask, getAllUsers, getUser } from './lib/actions';
+import { SetStateAction } from 'react';
 
 export default function Page() {
-  const [addTask, setAddTask] = useState<boolean>(false);
-  const [disableNameField, setDisableNameField] = useState<boolean>(false);
-  const [currentUser, setCurrentUser] = useState<string>('');
-  const [userTasks, setUserTasks] = useState<any[]>([]);
-  const [users, setUsers] = useState<any[]>([]);
-
-  useEffect(() => {
-    const loadUserTasks = async (userName: string) => {
-      if (userName) {
-        const tasks = await getUsersTask(userName);
-        console.log(tasks);
-        setUserTasks(tasks);
-      }
-    };
-    loadUserTasks(currentUser);
-  }, []);
-
-  useEffect(() => {
-    const loadUsers = async () => {
-      const usersData = await getAllUsers();
-      setUsers(usersData);
-    };
-    loadUsers();
-  }, []);
-
-  const enterName = async (formData: FormData) => {
-    try {
-      const userName = formData.get('user') as string;
-      await createUser({ userName });
-      setDisableNameField(true);
-      setCurrentUser(userName);
-    } catch (error) {
-      console.error('Error creating user:', error);
-    }
-  };
-
-  console.log(userTasks);
+  // const enterName = async (formData: FormData) => {
+  //   try {
+  //     const userName = formData.get('user') as string;
+  //     await createUser({ userName });
+  //   } catch (error) {
+  //     console.error('Error creating user:', error);
+  //   }
+  // };
 
   return (
     <div className="w-full">
-      <div className="flex justify-between">
-        <div className="flex">
-          <label htmlFor="selectUser">select user:</label>
-          <select name="selectUser" id="selectUser">
-            <option value=""></option>
-            {users.map((el) => (
-              <option key={el.id} value={el.name} defaultValue={currentUser}>
-                {el.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <form action={enterName} className="flex  items-center gap-2">
-          <label htmlFor="user">
-            <p>enter name</p>
-          </label>
-          <input
-            type="text"
-            name="user"
-            id="user"
-            className="border-[1px] border-add-btn-bg rounded-2xl px-2 text-sm"
-            placeholder="name"
-            disabled={disableNameField}
-          />
-          <button
-            type="submit"
-            className="w-4 h-4 rounded-full bg-add-btn-bg"
-            disabled={disableNameField}
-          >
-            <Image
-              src="/assets/images/Add_round_duotone.svg"
-              width={16}
-              height={16}
-              alt="add user"
-            />
-          </button>
-        </form>
-
-        <p>
-          current user: <span>{currentUser}</span>
-        </p>
-      </div>
+      <CreateUsersForm />
+      <div className="flex justify-between"></div>
       <ul>
-        {userTasks.map((el) => {
+        {/* {await getUsersTask().map((el) => {
           return (
             <li key={el.id}>
               <Task title={el.title} content={el.content} />
             </li>
           );
-        })}
+        })} */}
       </ul>
-
+      {/* 
       <NewTask setAddTask={setAddTask} />
-      {addTask && <Modal setAddTask={setAddTask} currentUser={currentUser} />}
+      {addTask && <Modal setAddTask={setAddTask} currentUser={currentUser} />} */}
     </div>
   );
 }
